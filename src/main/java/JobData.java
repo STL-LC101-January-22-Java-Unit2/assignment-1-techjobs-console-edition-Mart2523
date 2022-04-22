57665fe0b18ed92b86b3527de1baf4774667b65f
Cade M <you@example.com>
2022-04-21 19:38:35 -0500

    Trying to add search function

--- src/main/java/JobData.java
+++ src/main/java/JobData.java
@@ -5,10 +5,7 @@ import org.apache.commons.csv.CSVRecord;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.Reader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
+import java.util.*;
 
 /**
  * Created by LaunchCode
@@ -97,9 +94,18 @@ public class JobData {
 
         // load data, if not already loaded
         loadData();
+        ArrayList<HashMap<String, String>> searchValue = new ArrayList<>();
 
-        // TODO - implement this method
-        return null;
+        for (HashMap<String, String> jobs : allJobs){
+            for (String key : jobs.keySet()){
+                String aValue = jobs.get(key);
+                if (aValue.toLowerCase().contains((value.toLowerCase()))) {
+                    searchValue.add(jobs);
+                    break;
+                }
+            }
+        }
+        return searchValue;
     }
 
     /**
